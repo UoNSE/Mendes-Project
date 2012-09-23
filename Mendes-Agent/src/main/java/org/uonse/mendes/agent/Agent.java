b22aa17708b71d1f11157cc8cf822632a45d0641
Trent Houliston <trent@houliston.me>
2012-09-23 17:35:41 +1000

    Added some basic placeholder files

---
+++ Mendes-Agent/src/main/java/org/uonse/mendes/agent/Agent.java
@@ -0,0 +1,20 @@
+package org.uonse.mendes.agent;
+
+/**
+ * This is the main class for a Mendes Agent. It should connect to a server
+ * based on its configuration and begin taking requests from the server.
+ *
+ * @author Trent Houliston
+ */
+public class Agent
+{
+
+	/**
+	 * This is the main start point for the Mendes agent.
+	 *
+	 * @param args the arguments provided from the command line
+	 */
+	public static void main(String[] args)
+	{
+	}
+}
---
+++ Mendes-Agent/src/test/java/org/uonse/mendes/agent/AgentTest.java
@@ -0,0 +1,20 @@
+package org.uonse.mendes.agent;
+
+import org.junit.Test;
+
+/**
+ * This unit test is for testing the main Mendes Agent class.
+ *
+ * @author Trent Houliston
+ */
+public class AgentTest
+{
+
+	/**
+	 * Test the main Mendes agent class
+	 */
+	@Test
+	public void testAgent()
+	{
+	}
+}
