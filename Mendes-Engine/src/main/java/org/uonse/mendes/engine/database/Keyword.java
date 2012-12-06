1c2ae2a25bc69add61de98d654a7cba7079e1e08
Trent Houliston <trent@houliston.me>
2012-12-06 21:25:01 +1100

    Moved the keyword entity class into the engine package

--- Mendes-Engine/src/main/java/org/uonse/mendes/engine/database/Keyword.java
+++ Mendes-Engine/src/main/java/org/uonse/mendes/engine/database/Keyword.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the Lesser GNU General Public License
  * along with Project Mendes. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.uonse.mendes.server.database;
+package org.uonse.mendes.engine.database;
 
 import java.util.HashSet;
 import java.util.Set;
